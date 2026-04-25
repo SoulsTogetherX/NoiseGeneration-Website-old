1fe635bd5a56b1e281007d4e0b938eb3256355bb
SoulsTogetherX <xalvarez4444@gmail.com>
2026-04-24 21:45:27 -0700

    Learning Hugo more, settup menus, updated nav bar and css

--- ts/generalFunctionality.ts
+++ ts/generalFunctionality.ts
@@ -1,53 +1,32 @@
-//#region Screen Width Check
-const DESKTOP_CLASSNAME = "desktop"
-const MOBILE_CLASSNAME = "mobile"
-
-const MOBILE_MEDIA_QUERY = window.matchMedia("(max-width: 768px)")
-
-function handleLayoutChange() {
-  document.body.classList.remove(MOBILE_CLASSNAME)
-  document.body.classList.remove(DESKTOP_CLASSNAME)
-
-  if (MOBILE_MEDIA_QUERY.matches) {
-    document.body.classList.add(MOBILE_CLASSNAME)
-    return
-  }
-  document.body.classList.add(DESKTOP_CLASSNAME)
-}
-
-MOBILE_MEDIA_QUERY.addEventListener("change", handleLayoutChange)
-handleLayoutChange()
-//#endregion
-
 //#region Object Visiblity
 // Constants
-const VISIBLE_CLASSNAME = "visible"
-const ONESHOT_ATTRIBUTE_NAME = "oneShot"
+const VISIBLE_CLASSNAME = "visible";
+const ONESHOT_ATTRIBUTE_NAME = "oneShot";
 
 // Listener
 export class HTMLObserverElement extends HTMLElement {
   //#region     Private Variables
-  private _oneShot: boolean
-  private _oneShotFinished: boolean = false
+  private _oneShot: boolean;
+  private _oneShotFinished: boolean = false;
 
-  private _observer: IntersectionObserver | undefined
+  private _observer: IntersectionObserver | undefined;
   //#endregion
 
   //#region     Constructor
   constructor() {
-    super()
+    super();
 
     this._oneShot =
-      (this.getAttribute(ONESHOT_ATTRIBUTE_NAME) ?? false) === "true"
+      (this.getAttribute(ONESHOT_ATTRIBUTE_NAME) ?? false) === "true";
   }
   //#endregion
 
   //#region     Dom Enter/Exit
   protected connectedCallback(): void {
-    this._observer = new IntersectionObserver(this.onVisible)
+    this._observer = new IntersectionObserver(this.onVisible);
   }
   protected disconnectedCallback(): void {
-    this._observer = undefined
+    this._observer = undefined;
   }
   //#endregion
 
@@ -56,15 +35,15 @@ export class HTMLObserverElement extends HTMLElement {
     if (!this._oneShotFinished || !this._oneShot) {
       for (const element of elements) {
         if (element.isIntersecting) {
-          this._oneShotFinished = true
-          this.classList.add(VISIBLE_CLASSNAME)
-          return
+          this._oneShotFinished = true;
+          this.classList.add(VISIBLE_CLASSNAME);
+          return;
         }
       }
     }
-    this.classList.remove(VISIBLE_CLASSNAME)
+    this.classList.remove(VISIBLE_CLASSNAME);
   }
   //#endregion
 }
-customElements.define("visible-observer", HTMLObserverElement)
+customElements.define("visible-observer", HTMLObserverElement);
 //#endregion
